b4207bf25c890fe14a4e08e3fcfedbb53da466d6
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2022-02-03 23:58:47 +0200

    Serializer

--- demo/urls.py
+++ demo/urls.py
@@ -1,7 +1,11 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework import routers
+from .views import BookViewSet
+
+
+router = routers.DefaultRouter()
+router.register('books', BookViewSet)
 
 urlpatterns = [
-    path('first', views.first),
+    path('', include(router.urls)),
 ]
--- demo/views.py
+++ demo/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views import View
+from rest_framework import viewsets
 from .models import Book
-from django.shortcuts import render
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
 
 
-def first(request):
-    books = Book.objects.all()
-    return render(request, 'first_temp.html',
-                  {'data': 'this is a testing data from views',
-                   'books': books})
+
+
