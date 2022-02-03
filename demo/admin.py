f10c3ee293b9f89451a2a73704e5cc72500cc04c
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2022-02-04 01:23:55 +0200

    Many to Many relations

--- demo/admin.py
+++ demo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book, BookNumber, Character
+from .models import Book, BookNumber, Character, Author
 
 
 @admin.register(Book)
@@ -12,3 +12,4 @@ class BookAdmin(admin.ModelAdmin):
 
 admin.site.register(BookNumber)
 admin.site.register(Character)
+admin.site.register(Author)
--- demo/serializers.py
+++ demo/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Book, BookNumber, Character
+from .models import Book, BookNumber, Character, Author
 
 
 class BookNumberSerializer(serializers.ModelSerializer):
@@ -7,16 +7,25 @@ class BookNumberSerializer(serializers.ModelSerializer):
         model = BookNumber
         fields = ['id', 'isbn_10', 'isbn_13']
 
+
 class CharacterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Character
         fields = ['id', 'name']
 
 
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['id', 'name', 'surname']
+
+
 class BookSerializer(serializers.ModelSerializer):
     number = BookNumberSerializer(many=False)
     characters = CharacterSerializer(many=True)
+    authors = AuthorSerializer(many=True)
 
     class Meta:
         model = Book
-        fields = ['title', 'description', 'price', 'is_published', 'published', 'number','characters']
+        fields = ['title', 'description', 'price', 'is_published', 'published',
+                  'number', 'characters', 'authors']
